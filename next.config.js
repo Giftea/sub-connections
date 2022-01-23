786c97ebc1a93cc70e8346301341fbcec50a7b29
giftea <eleojogiftuhiene@gmail.com>
2022-01-23 22:07:02 +0100

    feat: fixed err

--- next.config.js
+++ next.config.js
@@ -11,11 +11,35 @@ const path = require('path');
   includePaths: [path.join(__dirname, 'styles')],
 };
  const reactStrictMode = true;
+ const typescript = {
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: true
+}
+//  function webpack(config) {
+//   config.module.rules.push({
+//     test: /\.svg$/i,
+//     issuer: /\.[jt]sx?$/,
+//     use: [
+//       {
+//         loader: '@svgr/webpack',
+//         options: {
+//           typescript: true,
+//           icon: true,
+//         },
+//       },
+//     ],
+//   });
 
+//   return config;
+// }
 
 module.exports = eslint;
 module.exports = images;
 module.exports = sassOptions;
 module.exports = reactStrictMode;
+module.exports = typescript
 
 
